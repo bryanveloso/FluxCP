56604d89263eded4e097630175b55c246bd439e0
Bryan Veloso <bryan@avalonstar.com>
2020-08-31 09:42:09 -0700

    Flip that.

--- themes/bootstrap/header.php
+++ themes/bootstrap/header.php
@@ -7,17 +7,13 @@
 		<meta name="description" content="">
 		<meta name="author" content="">
 
-		<?php if (isset($metaRefresh)): ?>
-		<meta http-equiv="refresh" content="<?php echo $metaRefresh['seconds'] ?>; URL=<?php echo $metaRefresh['location'] ?>" />
-		<?php endif ?>
+		<?php if (isset($metaRefresh)): ?><meta http-equiv="refresh" content="<?php echo $metaRefresh['seconds'] ?>; URL=<?php echo $metaRefresh['location'] ?>" /><?php endif ?>
 		<title><?php echo Flux::config('SiteTitle'); if (isset($title)) echo ": $title" ?></title>
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />
-		<link rel="stylesheet" href="<?php echo $this->themePath('css/flux.css') ?>" type="text/css" media="screen" title="" charset="utf-8" />
 		<link href="<?php echo $this->themePath('css/flux/unitip.css') ?>" rel="stylesheet" type="text/css" media="screen" title="" charset="utf-8" />
-		<?php if (Flux::config('EnableReCaptcha')): ?>
-        <script src='https://www.google.com/recaptcha/api.js'></script>
-		<?php endif ?>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<?php if (Flux::config('EnableReCaptcha')): ?><script src='https://www.google.com/recaptcha/api.js'></script><?php endif ?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo $this->themePath('css/flux.css') ?>" type="text/css" media="screen" title="" charset="utf-8" />
 		<link href="<?php echo $this->themePath('css/sticky-footer-navbar.css') ?>" rel="stylesheet">
 	</head>
 	<body>
