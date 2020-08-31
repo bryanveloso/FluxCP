88b04351193a1328beabd89bf3ec50d7acd97bdd
Bryan Veloso <bryan@avalonstar.com>
2020-08-31 09:26:20 -0700

    Fuck off IE9.

--- themes/bootstrap/header.php
+++ themes/bootstrap/header.php
@@ -1,4 +1,6 @@
-<?php if (!defined('FLUX_ROOT')) exit; ?>
+<?php if (!defined('FLUX_ROOT')) {
+    exit();
+} ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
@@ -10,31 +12,34 @@
 		<meta name="author" content="">
 
 		<?php if (isset($metaRefresh)): ?>
-		<meta http-equiv="refresh" content="<?php echo $metaRefresh['seconds'] ?>; URL=<?php echo $metaRefresh['location'] ?>" />
-		<?php endif ?>
-		<title><?php echo Flux::config('SiteTitle'); if (isset($title)) echo ": $title" ?></title>
+		<meta http-equiv="refresh" content="<?php echo $metaRefresh['seconds']; ?>; URL=<?php echo $metaRefresh[
+    'location'
+]; ?>" />
+		<?php endif; ?>
+		<title><?php
+  echo Flux::config('SiteTitle');
+  if (isset($title)) {
+      echo ": $title";
+  }
+  ?></title>
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />
-		<link rel="stylesheet" href="<?php echo $this->themePath('css/flux.css') ?>" type="text/css" media="screen" title="" charset="utf-8" />
-		<link href="<?php echo $this->themePath('css/flux/unitip.css') ?>" rel="stylesheet" type="text/css" media="screen" title="" charset="utf-8" />
+		<link rel="stylesheet" href="<?php echo $this->themePath(
+      'css/flux.css',
+  ); ?>" type="text/css" media="screen" title="" charset="utf-8" />
+		<link href="<?php echo $this->themePath(
+      'css/flux/unitip.css',
+  ); ?>" rel="stylesheet" type="text/css" media="screen" title="" charset="utf-8" />
 		<?php if (Flux::config('EnableReCaptcha')): ?>
 			<script src='https://www.google.com/recaptcha/api.js'></script>
-		<?php endif ?>
-		<!--[if IE]>
-		<link rel="stylesheet" href="<?php echo $this->themePath('css/flux/ie.css') ?>" type="text/css" media="screen" title="" charset="utf-8" />
-		<![endif]-->	
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link href="<?php echo $this->themePath('css/sticky-footer-navbar.css') ?>" rel="stylesheet">
+		<?php endif; ?>
 
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="<?php echo $this->themePath('css/sticky-footer-navbar.css'); ?>" rel="stylesheet">
 	</head>
 	<body>
-	
+
     <!-- Fixed navbar -->
-	<?php include $this->themePath('main/navbar.php', true) ?>
+	<?php include $this->themePath('main/navbar.php', true); ?>
 
 
 
@@ -42,24 +47,30 @@
 
 
 
-		<?php //include 'main/sidebar.php' ?>
+		<?php
+//include 'main/sidebar.php'
+?>
 
-		<?php //include 'main/loginbox.php' ?>
-					
+		<?php
+//include 'main/loginbox.php'
+?>
+
 			<?php if (Flux::config('DebugMode') && @gethostbyname(Flux::config('ServerAddress')) == '127.0.0.1'): ?>
 				<p class="notice">Please change your <strong>ServerAddress</strong> directive in your application config to your server's real address (e.g., myserver.com).</p>
-			<?php endif ?>
-								
+			<?php endif; ?>
+
 								<!-- Messages -->
-								<?php if ($message=$session->getMessage()): ?>
-									<p class="message"><?php echo htmlspecialchars($message) ?></p>
-								<?php endif ?>
-								
+								<?php if ($message = $session->getMessage()): ?>
+									<p class="message"><?php echo htmlspecialchars($message); ?></p>
+								<?php endif; ?>
+
 								<!-- Sub menu -->
-								<?php include $this->themePath('main/submenu.php', true) ?>
-								
+								<?php include $this->themePath('main/submenu.php', true); ?>
+
 								<!-- Page menu -->
-								<?php include $this->themePath('main/pagemenu.php', true) ?>
-								
+								<?php include $this->themePath('main/pagemenu.php', true); ?>
+
 								<!-- Credit balance -->
-								<?php //if (in_array($params->get('module'), array('donate', 'purchase'))) include 'main/balance.php' ?>
+								<?php
+//if (in_array($params->get('module'), array('donate', 'purchase'))) include 'main/balance.php'
+?>
